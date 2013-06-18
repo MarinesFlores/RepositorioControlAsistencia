fdee9159ec619fa5171382137b9efa86ca6ac572
alexz1520 <alexz1520@gmail.com>
2013-06-17 20:26:09 -0400

    Clase Implementada con los metodos
    Insertar
    Actualizar
    Eliminar
    Listar
    Obtener
    ListarActivos

--- Proyecto Asistencia/Asistencia/Planilla/App_Code/Personal/Datos/TipoContrato.cs
+++ Proyecto Asistencia/Asistencia/Planilla/App_Code/Personal/Datos/TipoContrato.cs
@@ -8,6 +8,104 @@ namespace Personal.Datos
     public class TipoContrato
     {
 
-       
+       #region Variables privadas
+
+        private System.Int32 id_;
+        private System.String descripcion_;
+        private System.Int32 activo_;
+
+        #endregion
+
+        #region Propiedades publicas
+
+        public System.Int32 id
+        {
+            get
+            {
+                return id_;
+            }
+            set
+            {
+                id_ = value;
+            }
+        }
+        public System.String descripcion
+        {
+            get
+            {
+                return descripcion_.Trim ();
+            }
+            set
+            {
+                descripcion_ = value;
+            }
+        }
+        public System.Int32 activo
+        {
+            get
+            {
+                return activo_;
+            }
+            set
+            {
+                activo_ = value;
+            }
+        }
+
+        #endregion
+
+        #region Metodos publicos
+
+        public System.Int32 Insertar ()
+        {
+            if ( Conexion.Conexion.Ejecutar ( "insert into TipoContrato values(?, ?)", descripcion, activo ) == 0 )
+                return -1;
+            System.Data.DataTable t = Conexion.Conexion.Obtener ( "select max(id) from TipoContrato" );
+            return System.Convert.ToInt32 ( t.Rows[0][0] );
+        }
+
+        public void Actualizar ()
+        {
+            Conexion.Conexion.Ejecutar ( "update TipoContrato set descripcion=?, activo=? where id=?", descripcion, activo, id );
+        }
+
+        public void Eliminar ()
+        {
+            // Conexion.Conexion.Ejecutar("delete from tipoContrato where id=?", id);
+            Conexion.Conexion.Ejecutar ( "update tipoContrato set activo=0 where id=?", id );
+        }
+
+        public System.Data.DataTable Listar ()
+        {
+            return Listar ( "" );
+        }
+
+        public System.Data.DataTable Listar ( string condicion, params object[] args )
+        {
+            string s = "select * from TipoContrato " + ( !string.IsNullOrEmpty ( condicion ) ? "where " + condicion : "" );
+            return Conexion.Conexion.Obtener ( s, args );
+        }
+
+        public System.Data.DataRow Obtener ()
+        {
+            System.Data.DataTable t = Conexion.Conexion.Obtener ( "select * from TipoContrato where id=?", id );
+            if ( t.Rows.Count > 0 )
+            {
+                id = System.Convert.ToInt32 ( t.Rows[0][0] );
+                descripcion = System.Convert.ToString ( t.Rows[0][1] );
+                activo = System.Convert.ToInt32 ( t.Rows[0][2] );
+                return t.Rows[0];
+            }
+            return null;
+        }
+
+
+        public System.Data.DataTable ListarActivos ( string condicion, params object[] args )
+        {
+            string s = "select * from TipoContrato where activo=1" + ( !string.IsNullOrEmpty ( condicion ) ? "where " + condicion : "" );
+            return Conexion.Conexion.Obtener ( s, args );
+        }
+
+        #endregion
     }
 }
